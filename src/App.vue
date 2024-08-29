ae72292d71922159554143fa065b8d1eb3ca1340
myusrila23 <myusrila23@gmail.com>
2024-08-29 02:25:15 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <label>Original Image: </label>
+    <input type="file">
+    <div style="border: 1px solid white;">
+      <img ref>     
+    </div>
+  </div>
+  <div>
+
+  </div>
+</template>
+
+<style scoped></style>
